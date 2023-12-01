403b6ae31254d2e3c900a1643b0fffa44dd27493
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2023-12-01 13:09:02 +0400

    fixed issues

--- src/client.js
+++ src/client.js
@@ -11,28 +11,28 @@ const updateAndfetchUserLikesUrl = `${stageUrl}/userlikes`
 const likeUrl = `${stageUrl}/items/like`
 const dislikeUrl = `${stageUrl}/items/dislike`
 
-
 const checkStatus = response => {
-    if( response &&
-        response.status &&
-        response.status >= 200 &&
-        response.status < 300) {
+  console.log (response.status)
+    if(response.status === 200) {
+      console.log("2")
         return response;
     }
+    console.log("3")
     const error = new Error(response.status)
     error.response = response
+    console.log("4")
     return Promise.reject(error)
 }
 
-const logApiCall = (method, url, type, headers, params, body) => {
-    console.log(`Method ${method} has been invoked. Url ${url} | request type ${type} | headers ${headers} | params ${params} | body ${body}`)
+const logApiCall = (name, url, type, headers, params, body) => {
+    console.log(`Method ${name} has been invoked. Url ${url} | request type ${type} | headers ${JSON.stringify(headers)} | params ${JSON.stringify(params)} | body ${JSON.stringify(body)}`)
 }
 
 export const fetchLikesByUser = async (userId, headers) => {
 
     const url = `${updateAndfetchUserLikesUrl}/${userId}`;
 
-    logApiCall(self.name, url, "GET", headers)
+    logApiCall("fetchLikesByUser", url, "GET", headers)
 
     const response = await axios.get(url, { headers });
     return checkStatus (response);
@@ -42,37 +42,44 @@ export const updateLikesByUser = async (userId, likedItems, headers) => {
 
     const url = `${updateAndfetchUserLikesUrl}/${userId}`;
     const requestBody = {
-        "userId" : userId,
-        "likedItems": likedItems
+        userId : userId,
+        likedItems: likedItems
       };
 
-    logApiCall (self.name, url, "POST", headers, "", requestBody)
+    logApiCall ("updateLikesByUser", url, "POST", headers, "", requestBody)
 
-    const response = await axios.post(url, requestBody, {
-        headers
-    })
+    const response = await axios.post(
+      url,
+      requestBody,
+      {headers}
+      )
     return checkStatus (response);
 }
 
 
 export const likeItem = async (itemId, headers) => {
 
-    logApiCall(self.name, likeUrl, "POST", headers, { "itemId" : itemId } )
+    logApiCall("likeItem", likeUrl, "POST", headers, { itemId : itemId } )
 
-      const response = await axios.post(likeUrl, {
+      const response = await axios.post(
+        likeUrl,
+        null,
+        {
         headers,
-        params: { "itemId" : itemId },
+        params: { itemId : itemId }
       });
       return checkStatus (response);
   };
 
 export const dislikeItem = async (itemId, headers) => {
 
-    logApiCall(self.name, dislikeUrl, "POST", headers, { "itemId" : itemId } )
+    logApiCall("dislikeItem", dislikeUrl, "POST", headers, {  itemId : itemId } )
 
-    const response = await axios.post(dislikeUrl, {
+    const response = await axios.post(
+      dislikeUrl,
+      null, {
       headers,
-      params: { "itemId" : itemId },
+      params: {  itemId : itemId }
     });
     return checkStatus (response);
 };
@@ -80,7 +87,7 @@ export const dislikeItem = async (itemId, headers) => {
 export const fetchAllItemsAndLikes = async () => {
     const url = `${fetchAllLikesUrl}`;
 
-    logApiCall(self.name, url, "GET", unauthHeaders)
+    logApiCall("fetchAllItemsAndLikes", url, "GET", unauthHeaders)
 
     const response = await axios.get(url, {unauthHeaders});
     return checkStatus (response);
