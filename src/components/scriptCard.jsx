c34f1b522bedbe1f860ef203e5640d24049efaee
Vachagan <113465348+Metax7@users.noreply.github.com>
2023-12-02 13:45:53 +0400

    fixing a bug related to multiple likes

--- src/components/scriptCard.jsx
+++ src/components/scriptCard.jsx
@@ -7,47 +7,51 @@ import {
   Typography,
   Button,
 } from "@material-tailwind/react";
-import { BiSolidLike } from "react-icons/bi";
+// import { BiSolidLike } from "react-icons/bi";
 import { useEffect, useState } from "react";
-import { PuffLoader } from "react-spinners";
-import { 
-  likeItem,
-  dislikeItem,
-  updateLikesByUser 
-} from "../client"
+// import { PuffLoader } from "react-spinners";
+import { likeItem, dislikeItem, updateLikesByUser } from "../client";
 import { infoNotification } from "./Notifications";
+import LikeButton from "./likeButton";
 
-export default function ScriptCard({  
+export default function ScriptCard({
   card,
   userId,
-  userLikes, 
-  items, 
-  headers, 
-  likeHandler, 
+  userLikes,
+  items,
+  headers,
+  likeHandler,
   dislikeHandler,
-  isAuthorized
+  isAuthorized,
 }) {
   const [like, setLike] = useState(0);
   const [isLiked, setIsLiked] = useState(false);
   const [loading, setLoading] = useState(false);
-  useEffect(()=>{
-    setUpLikes(card.id, items, userLikes)
-  },[])
+  useEffect(() => {
+    setUpLikes(card.id, items, userLikes);
+  }, []);
 
   const setUpLikes = (cardId, items, userLikes) => {
     setLoading(true);
-    console.log(`CARD ID: ${cardId}`)
-    console.log(`ITEMS: ${JSON.stringify(Array.from(items))}`)
-    console.log(`USERLIKES: ${JSON.stringify(Array.from(userLikes))}`)
+    console.log(`CARD ID: ${cardId}`);
+    console.log(`ITEMS: ${JSON.stringify(Array.from(items))}`);
+    console.log(`USERLIKES: ${JSON.stringify(Array.from(userLikes))}`);
     try {
       const itemArray = Array.from(items);
       const userLikesArray = Array.from(userLikes);
 
-      setLike(Number(itemArray.find((item) => item.ItemId === cardId.toString()).Likes));
+      setLike(
+        Number(
+          itemArray.find((item) => item.ItemId === cardId.toString()).Likes
+        )
+      );
 
-      if (userLikesArray !== undefined && userLikesArray.includes(cardId.toString())) {
-        setLike((prev) => prev + 1)
-        setIsLiked(true)
+      if (
+        userLikesArray !== undefined &&
+        userLikesArray.includes(cardId.toString())
+      ) {
+        setLike((prev) => prev + 1);
+        setIsLiked(true);
       }
     } catch (error) {
       console.error(
@@ -57,52 +61,69 @@ export default function ScriptCard({
     } finally {
       setLoading(false);
     }
-}
+  };
 
   const likeButtonClick = async (itemId) => {
-    console.log(`BEFORE HEADERS ${headers} itemId ${itemId}`)
-    if (!isAuthorized) return infoNotification("To like or dislike you need to be authenticated", null,4)
+    console.log(`BEFORE HEADERS ${headers} itemId ${itemId}`);
+    if (!isAuthorized)
+      return infoNotification(
+        "To like or dislike you need to be authenticated",
+        null,
+        4
+      );
     setLoading(true);
-    if (!isLiked){
+    if (!isLiked) {
       try {
-        const response = await likeItem(itemId, headers)
-        likeHandler(itemId)
-        const responseOfUpdate = await updateLikesByUser(userId, Array.from(userLikes), headers)
-        setLike((prev) => prev + 1)
-      }  catch (error) {
+        const response = await likeItem(itemId, headers);
+        likeHandler(itemId);
+        const responseOfUpdate = await updateLikesByUser(
+          userId,
+          Array.from(userLikes),
+          headers
+        );
+        setLike((prev) => prev + 1);
+      } catch (error) {
         console.error(
           "Не удалось поставить лайк. Статус:",
           error.response || error.message || error
         );
-        dislikeHandler(itemId)
+        dislikeHandler(itemId);
       } finally {
         setLoading(false);
-      } 
-        console.log("Лайк успешно поставлен!");
-    }
-    else {
+      }
+      console.log("Лайк успешно поставлен!");
+    } else {
       try {
-        const response = await dislikeItem(itemId, headers)
-        dislikeHandler(itemId)
-        const responseOfUpdate = await updateLikesByUser(userId, Array.from(userLikes), headers)
-        setLike((prev) => prev - 1)
-
-      }  catch (error) {
+        const response = await dislikeItem(itemId, headers);
+        dislikeHandler(itemId);
+        const responseOfUpdate = await updateLikesByUser(
+          userId,
+          Array.from(userLikes),
+          headers
+        );
+        setLike((prev) => prev - 1);
+      } catch (error) {
         console.error(
           "Не удалось поставить дизлайк. Статус:",
           error.response || error.message || error
         );
-        likeHandler(itemId)
+        likeHandler(itemId);
       } finally {
         setLoading(false);
-      } 
-        console.log("Лайк успешно поставлен!");
-
+      }
+      console.log("Лайк успешно поставлен!");
     }
-    console.log("AFTER")
+    console.log("AFTER");
     setIsLiked(!isLiked);
   };
 
+  const handleButtonClick = (e) => {
+    if (loading) {
+      e.stopPropagation();
+    } else {
+      likeButtonClick(card.id);
+    }
+  };
 
   return (
     <Card className="mt-6 shadow-2xl bg-slate-300 hover:scale-95 transition-all duration-200">
@@ -119,22 +140,12 @@ export default function ScriptCard({
           <Button>SEE MORE</Button>
         </Link>
         <div>
-          <button
-            className={
-              "" +
-              (isLiked
-                ? "flex items-center border font-bold rounded-md p-2 space-x-2 border-[#111827] text-blue-600"
-                : "flex items-center border font-bold rounded-md p-2 space-x-2 border-[#111827]")
-            }
-            onClick={()=>likeButtonClick(card.id)}
-          >
-            <BiSolidLike />
-            {loading ? (
-              <PuffLoader color="#111827" loading={loading} size={16} />
-            ) : (
-              <span>{like}</span>
-            )}
-          </button>
+          <LikeButton
+            isLiked={isLiked}
+            loading={loading}
+            like={like}
+            onClick={handleButtonClick}
+          />
         </div>
       </CardFooter>
     </Card>
