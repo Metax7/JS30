a64e626eb9e689152cda2cfb369425eee3433a40
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2023-12-07 21:08:49 +0400

    documented utils methods

--- src/authHandler.js
+++ src/authHandler.js
@@ -5,6 +5,21 @@ const infoUrlSuffix = '/oauth2/userInfo'
 const oauth2Headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
 const codeChallengeMethod = 'S256'
 
+// TODO: Wrap everything in TryCatch
+
+/**
+ * 
+ * @param {string} IdentityProvider - name of Identity Provider such as Google, Facebook etc
+ * @param {string} clientId - id of Application Client registered in Cognito Userpool
+ * @param {string} redirectUri - uri to be redirected - The URL where the authentication server redirects the browser after Amazon Cognito authorizes the user.
+ * @param {string} scope - Can be a combination of any system-reserved scopes or custom scopes that are associated with a client. Scopes must be separated by spaces.
+ * @param {string} state - Additional computed value to guard against CSRF attacks.
+ * @param {string} codeChallenge - The challenge that you generated from the code_verifier. Use for PKCE
+ * 
+ * @see https://datatracker.ietf.org/doc/html/rfc7636
+ * 
+ * @returns {string} callback url with Authorization Code
+ */
 export const getOpenSamlUrl = (
     IdentityProvider,
     clientId,
@@ -12,8 +27,7 @@ export const getOpenSamlUrl = (
     scope,
     state,
     codeChallenge) => {
-// https://docs.aws.amazon.com/cognito/latest/developerguide/authorization-endpoint.html
-// FIXME: add STATE ,                           code_challenge_method=S256&code_challenge=CODE_CHALLENGE
+
 const authorizeUrlSuffix =  '/oauth2/authorize'
 const params = {}
 params.append("identity_provider", IdentityProvider)
@@ -31,20 +45,35 @@ if (codeChallenge !== undefined && codeChallenge !== null) {
 
 
 getOAuth(authorizeUrlSuffix, params)
-//     
+    
 // const ff = `${cognitoHost}/oauth2/authorize?identity_provider=${IdentityProvider}
 // &response_type=CODE&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`
     
 }
+/**
+ * 
+ * @param {*} clientId - id of Application Client registered in Cognito Userpool
+ * @param {*} authorizationCode - code of grant_type to be exchanged to Authorization Token
+ * @param {*} redirectUri - Must be the same redirect_uri that was used to get authorization_code in /oauth2/authorize.
+ * @param {*} codeVerifier - The proof key. Required if the authorization code was requested with PKCE.
+ * 
+ * @returns {string}  {
+                        "access_token":"eyJra1example", 
+                        "id_token":"eyJra2example",
+                        "refresh_token":"eyJj3example",
+                        "token_type":"Bearer", 
+                        "expires_in":3600
+                            }
+ */
 export const xChange = async (
     clientId,
-    authorization_code,
+    authorizationCode,
     redirectUri,
     codeVerifier) => {
     const body = {}
     body.append('grant_type','authorization_code')
     body.append('client_id', clientId)
-    body.append('code', authorization_code)
+    body.append('code', authorizationCode)
     body.append('redirect_uri', redirectUri)
 
     if(codeVerifier!==undefined && codeVerifier!==null) {
@@ -52,16 +81,48 @@ export const xChange = async (
     }
     postOAuth(tokenUrlSuffix, oauth2Headers, body)
 }
-export const refreshToken = async (clientId, token) => {
+
+/**
+ * 
+ * @param {*} clientId - id of Application Client registered in Cognito Userpool
+ * @param {*} refreshToken - refresh_token to be refresh acced and it_token pair.
+ * 
+ * @returns {object} { 
+                        "access_token":"eyJra1example", 
+                        "id_token":"eyJra2example",
+                        "token_type":"Bearer", 
+                        "expires_in":3600
+                        }
+ */
+export const refreshToken = async (clientId, refreshToken) => {
     const body = {}
     body.append('grant_type','refresh_token')
     body.append('client_id', clientId)
-    body.append('refresh_token', token)
+    body.append('refresh_token', refreshToken)
     
     postOAuth(tokenUrlSuffix, oauth2Headers, body)
 }
 
-export const getUserInfo = async (token) => {
+export const revokeToken = async () => {
+    const revokeUrlSuffix = '/oauth2/revoke'
+    const body = {
+    }
+    body.append('token', token)
+    body.append('client_id', clientId)
+
+    postOAuth(revokeUrlSuffix,oauth2Headers,body)
+}
+
+/**
+ * Get user info.
+ * @param {string} accessToken 
+ * 
+ * @returns {object} {
+            "sub": "userId",
+            "email": "bob@example.com",
+}
+ */
+export const getUserInfo = async (accessToken) => {
     const headers = {}
     headers.append('Content-Type', 'application/x-amz-json-1.1')
     headers.append('Authorization', `Bearer ${token}`)
@@ -69,6 +130,12 @@ export const getUserInfo = async (token) => {
     postOAuth(infoUrlSuffix, headers)
 }
 
+
+/**
+ * extract Autorization Code from callback uri
+ * @param {string} responseUrl 
+ * @returns {string} - Authorization Code that is intended to be exchanged to access-, refresh, and id-tokens 
+ */
 export const extractCode = responseUrl => {
     // TODO: Write extracter
     return "NOT IMPLEMENTED YET"
@@ -77,4 +144,9 @@ export const extractCode = responseUrl => {
 export const getChallengePKCE = () => {
     // TODO: Implement
     return "get Challenge NOT IMPLEMENTED YET"
+}
+
+export const getState = () => {
+    // TODO: Implement
+    return "getState NOT IMPLEMENTED YET"
 }
--- src/pages/auth/saml.jsx
+++ src/pages/auth/saml.jsx
@@ -1,25 +1,34 @@
 
 const clientId = import.meta.env.VITE_JS30_IDP_CLIENT_ID
 const domain = import.meta.env.VITE_JS30_FULL_DOMAIN
-const redirectUri = domain === 'local' ? 'http://localhost:5173' : domain.startsWith('dev') ? `http://${domain}` : `https://${domain}` 
+const redirectUri = domain === 'local' ? 'https://localhost:5173' : domain.startsWith('dev') ? `http://${domain}` : `https://${domain}` 
 const IdP = 'Google'
-const IdPScope = 'aws.cognito.signin.user.admin email openid'
+const IdPScope = "aws.cognito.signin.user.admin email openid"
 import { 
   getOpenSamlUrl,
   refreshToken,
   xChange,
   extractCode, 
   getUserInfo,
-  getChallengePKCE
+  getChallengePKCE,
+  getState,
+  revokeToken
 } from "../../authHandler"
-
+/**
+ * Sign in with Google Account
+ *  @returns {object} userData conisting of {string} idToken and {string} userId
+ * 
+ */
 const signInWithGoogle = () => {
   const authUrl = getOpenSamlUrl(IdP,clientId,redirectUri,IdPScope)
   const auth_code = extractCode(authUrl)
-  const token = xChange(clientId,auth_code,redirectUri)
-  const userId = getUserInfo(token).sub
+  const authData = xChange(clientId,auth_code,redirectUri)
+  const idToken = authData.id_token
+  const refreshToken = authData.refresh_token
+  const accessToken = authData.access_token
+  const userId = getUserInfo(accessToken).sub
   const userData = {
-    'token': token,
+    'token': idToken,
     'userId': userId
   }
   return userData
